cd0db6abf527004ddb97d5d55989a48ef1783b67
fogset <zhangth1990@hotmail.com>
2021-05-07 19:20:20 -0700

    fixing alignment error

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,26 +7,26 @@ function Footer() {
     return (
         <div className='footer-container'>
             <section className='footer-subscription'>
-                <p className="footer-subscription-heading">
+                <p className='footer-subscription-heading'>
                     Join the Adventure newsletter to receive our best vacation deals
-                </p>
-                <p className="footer-subscription-text">
-                    You can unsubscribe at any time
-                </p>
-                <div className="input-areas">
+        </p>
+                <p className='footer-subscription-text'>
+                    You can unsubscribe at any time.
+        </p>
+                <div className='input-areas'>
                     <form>
                         <input
-                            type="email"
-                            name="email"
-                            placeholder="Your Email"
-                            className="footer-input"
+                            className='footer-input'
+                            name='email'
+                            type='email'
+                            placeholder='Your Email'
                         />
-                        <Button buttonStyle="btn--outline">SUBSCRIBE</Button>
+                        <Button buttonStyle='btn--outline'>Subscribe</Button>
                     </form>
                 </div>
             </section>
             <div className="footer-links">
-                <div className="footer-links-wrapper">
+                <div className="footer-link-wrapper">
                     <div class='footer-link-items'>
                         <h2>About Us</h2>
                         <Link to='/sign-up'>How it works</Link>
@@ -47,7 +47,7 @@ function Footer() {
                     <div class='footer-link-items'>
                         <h2>Videos</h2>
                         <Link to='/'>Submit Video</Link>
-                        <Link to='/'>Ambassadors</Link>F
+                        <Link to='/'>Ambassadors</Link>
                         <Link to='/'>Agency</Link>
                         <Link to='/'>Influencer</Link>
                     </div>
@@ -60,6 +60,7 @@ function Footer() {
                     </div>
                 </div>
             </div>
+
             <section className="social-media">
                 <div className="social-media-wrap">
                     <div className="footer-logo">
@@ -111,7 +112,6 @@ function Footer() {
                         <i class='fab fa-linkedin' />
                     </Link>
                 </div>
-
             </section>
         </div>
     )
